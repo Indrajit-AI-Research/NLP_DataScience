86e18c28aced8188d07eefc1b7282236aedd5e72
IsinghGitHub <Indrajitsingh37@gmail.com>
2021-01-31 03:54:16 +0530

    commit

--- gatsby-simplefolio/src/mock/data.js
+++ gatsby-simplefolio/src/mock/data.js
@@ -10,8 +10,8 @@ export const headData = {
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
+  name: 'Indrajit Singh',
+  subtitle: 'NLP Researcher',
   cta: '',
 };
 
